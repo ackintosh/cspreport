edd490e479ca08caf1116fb2b51917838a0bd768
akihito.nakano <sora.akatsuki@gmail.com>
2017-05-23 21:24:57 +0900

    Add http header

--- app.rb
+++ app.rb
@@ -2,5 +2,6 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/' do
-  'Hello CSP Report! <br /><img src="http://ackintosh.github.io/assets/images/favicon.gif" />'
+  body = 'Hello CSP Report! <br /><img src="http://ackintosh.github.io/assets/images/favicon.gif" />'
+  [200, {'Content-Security-Policy-Report-Only' => 'block-all-mixed-content; report-uri https://cspreport.herokuapp.com/csp-report'}, body]
 end
