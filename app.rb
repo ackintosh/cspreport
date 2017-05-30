023d42f0fe27faf195bc74c11edbfbf11de81e28
akihito.nakano <sora.akatsuki@gmail.com>
2017-05-30 09:19:54 +0900

    report-to

--- app.rb
+++ app.rb
@@ -1,7 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'json'
 
 get '/' do
   body = 'Hello CSP Report! <br /><img src="http://ackintosh.github.io/assets/images/favicon.gif" />'
-  [200, {'Content-Security-Policy-Report-Only' => 'block-all-mixed-content; report-uri https://cspreport.herokuapp.com/csp-report'}, body]
+  # [200, {'Content-Security-Policy-Report-Only' => 'block-all-mixed-content; report-uri https://cspreport.herokuapp.com/csp-report'}, body]
+  report_to = {
+      url: 'https://cspreport.herokuapp.com/csp-report',
+      group: 'cspreport',
+      'max-age' => 10886400,
+  }
+  # p report_to.to_json
+  header = {
+      'Report-To' => report_to.to_json,
+      'Content-Security-Policy-Report-Only' => 'block-all-mixed-content; report-uri https://cspreport.herokuapp.com/csp-report; report-to cspreport',
+  }
+  [200, header, body]
 end
